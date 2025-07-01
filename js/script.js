0e3b055d6324da86dbd64a1554b21e94d60dbd3f
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-01 15:37:17 +0200

    [upd] controlli

--- js/script.js
+++ js/script.js
@@ -5,15 +5,18 @@ let discount = 0;
 let price = 0.21 * kmToGo;
 
 //logica
-if (age < 18) {
-  discount = price * 20 / 100;
-} else if (age > 65) {
-  discount = price * 40 / 100;
-}
-
 if (isNaN(kmToGo) || isNaN(age)) {
   alert('ATTENZIONE: devi inserire dei numeri');
+  console.log(`non hai inserito dei numeri`)
+} else {
+  if (age < 18) {
+    discount = price * 20 / 100;
+  } else if (age > 65) {
+    discount = price * 40 / 100;
+  }
+  price = price - discount;
+  console.log(price.toFixed(2));
 }
 
-price = price - discount;
-console.log(price.toFixed(2));
+
+
