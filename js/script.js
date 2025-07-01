179511f80ceeb8bb7c8e0d89342639787d9882b3
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-01 14:05:44 +0200

    [upd] discount minorenni

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 //dichiarazione variabili
 const kmToGo = parseInt(prompt('Inserisci quanti KM vuoi percorrere'));
 const age = parseInt(prompt('Inserisci la tua età per un eventuale sconto'));
+let discount;
+let price = 0.21 * kmToGo;
+
+if (age < 18) {
+  discount = price * 20 / 100;
+  price = price - discount;
+}
+
+console.log(price);
